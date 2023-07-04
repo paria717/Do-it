9a5f5ecfc2edeb27e7ad5eac55629c04fa289c43
paria77 <paria77.gh@gmail.com>
2023-07-04 13:11:02 +0400

    createwidget and create listtasks

--- lib/pages/tasks_page.dart
+++ lib/pages/tasks_page.dart
@@ -8,63 +8,104 @@ class TasksPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          elevation: 1,
-          child: const Icon(Icons.add, color: Colors.white),
-        ),
+        floatingActionButton: MyFloatingActionButton(),
         body: Stack(children: [
-          Container(
-            width: Get.width,
-            height: Get.height / 4,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-              ),
-              color: AppColors.appbarcolor,
-            ),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.arrow_back_ios_new_rounded,
-                    color: Colors.white,
-                  )),
-              SizedBox(height: Get.height / 20),
-              Center(
-                child: RichText(
-                  text: TextSpan(
-                      text: 'Think How To',
-                      style: GoogleFonts.dancingScript(
-                          fontSize: 30, color: Colors.white),
-                      children: [
-                        TextSpan(
-                            text: ' Do it',
-                            style: GoogleFonts.dancingScript(
-                                fontWeight: FontWeight.w900,
-                                fontSize: 30,
-                                color: const Color.fromARGB(255, 84, 24, 189))),
-                      ]),
-                ),
-              )
-            ]),
-          ),
-          Container(
-            color: AppColors.backgroungcolors,
-            height: Get.height,
-            width: Get.width,
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return const Card();
-              },
-            ),
-          ),
+          MyCustomAppBar(),
+          MyTasksList(),
         ]),
       ),
+    );
+  }
+}
+
+class MyFloatingActionButton extends StatelessWidget {
+  const MyFloatingActionButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: AppColors.appbarcolor,
+      onPressed: () {},
+      elevation: 1,
+      child: const Icon(Icons.add, color: Colors.white),
+    );
+  }
+}
+
+class MyTasksList extends StatelessWidget {
+  const MyTasksList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.backgroungcolors,
+      height: Get.height,
+      width: Get.width,
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const Card(
+            color: AppColors.cardcolors,
+            elevation: 16,
+            child: ListTile(
+                title: Text(
+                  "data",
+                ),
+                subtitle: Text('data')),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyCustomAppBar extends StatelessWidget {
+  const MyCustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Get.width,
+      height: Get.height / 4,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+        ),
+        color: AppColors.appbarcolor,
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back_ios_new_rounded,
+              color: Colors.white,
+            )),
+        SizedBox(height: Get.height / 20),
+        Center(
+          child: RichText(
+            text: TextSpan(
+                text: 'Think How To',
+                style: GoogleFonts.dancingScript(
+                    fontSize: 30, color: Colors.white),
+                children: [
+                  TextSpan(
+                      text: ' Do it',
+                      style: GoogleFonts.dancingScript(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 30,
+                          color: const Color.fromARGB(255, 84, 24, 189))),
+                ]),
+          ),
+        )
+      ]),
     );
   }
 }
